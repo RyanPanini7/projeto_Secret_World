768d63a2c0fc10e011be7f3518f4c9581bf3bc71
ryanpanini7 <ryanmpanini@gmail.com>
2025-06-03 20:26:04 -0300

    bug de css CORRIGIDO

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import "./App.css";
+import "./App.css"
+
+import StartScreen from "./components/StartScreen";
 
 function App() {
   return (
     <div className="App">
-      <h2>Secret World</h2>
+      <StartScreen/>
     </div>
   )
-};
+}
 
 export default App
---
+++ src/components/StartScreen.jsx
@@ -0,0 +1,13 @@
+import "./StartScreen.css"
+
+const StartScreen = () => {
+  return (
+    <div className="start">
+        <h1>Secret World</h1>
+        <p>Clique no botão abaixo para começar!</p>
+        <button>Iniciar jogo</button>
+    </div>
+  )
+}
+
+export default StartScreen
